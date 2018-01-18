6f541e1c61d8082d3924d63eab2062626725e3a3
Pekko Metsä <pekko.metsa@helsinki.fi>
2018-01-18 20:41:14 +0200

    Now having a config file hv.conf.  See doc/hv.conf.example.

--- README.md
+++ README.md
@@ -2,14 +2,15 @@
 
 The power supplies from Technix http://www.technix-hv.com can be
 controlled via RS232 bus using a simple ASCII based protocol.  The aim
-of the hvcontrol project is to implement a Python3 control library
-for the Technix SR 5kW to 10kW series
+of the hvcontrol project is to implement a Python3 control library for
+the Technix SR 5kW to 10kW series
 http://www.technix-hv.com/technix/products/HV-generators/sr-5000-to-10000-watts.
-We are testing the implementation with Technix SR100kV-5kW, having a
+We are testing the implementation with Technix SR100kV-5kW, which has
 negative polarity (i.e. the voltage range from ground potential 0.0 V
 down to -100.0 kV).
 
-The dependencies of the library are tried to keep simple.  Currently
-only the pyserial module is needed, the versions used for testing are
-pySerial v3.2.1 from Debian python3-serial package v3.2.1-1 and v3.4
-from PyPi.  Developers are using both Python v3.5 and v3.6.
+The dependencies of the library are tried to keep simple.  Currently,
+besides of the standard library, only the pyserial module is needed,
+the versions used for testing are pySerial v3.2.1 from Debian
+python3-serial package v3.2.1-1 and v3.4 from PyPi.  Developers are
+using both Python v3.5 and v3.6.
--- lib/technix.py
+++ lib/technix.py
@@ -1,4 +1,61 @@
-def send_command(command):
+#!/usr/bin/env python3
+"""
+Module for controlling Technix SR high voltage PSU via serial bus
+"""
+
+import configparser
+
+configfile = 'hv.conf'
+cfg = configparser.ConfigParser()
+cfg.read(configfile)
+
+MAX_VOLTAGE   = float(cfg.get('default', 'MAX_VOLTAGE'  ))
+MAX_CURRENT   = float(cfg.get('default', 'MAX_CURRENT'  ))
+VOLTAGE_LIMIT = float(cfg.get('default', 'VOLTAGE_LIMIT'))
+CURRENT_LIMIT = float(cfg.get('default', 'CURRENT_LIMIT'))
+
+def set_voltage(U):
+    pass
+
+def set_current(I):
+    pass
+
+def get_status():
+    pass
+
+def get_voltage():
+    pass
+
+def get_current():
+    pass
+
+def inc_voltage(dU):
+    pass
+
+def inc_current(dI):
+    pass
+
+def hv_on():
+    pass
+
+def hv_off():
+    pass
+
+def get_voltagelimit():
+    return VOLTAGE_LIMIT
+
+def get_currentlimit():
+    return CURRENT_LIMIT
+
+def get_maxvoltage():
+    return MAX_VOLTAGE
+
+def get_maxcurrent():
+    return MAX_CURRENT
+
+
+
+def __send_command(command):
     """Checks whether the 'command' is valid, and that the resulting
     voltage and current values will still be within the allowed
     limits, and sends the command to the serial bus if everything is
@@ -8,6 +65,7 @@ def send_command(command):
     print(command)
     return (0.0, 0.0)
 
+
 if __name__=='__main__':
-    send_command("Testing 1-2-3")
+    __send_command("Testing 1-2-3")
 
